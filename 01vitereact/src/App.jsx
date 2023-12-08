96c4ec70c39b72317990e676256b971e2794007c
Anushka8855 <anushka.singh0604@gmail.com>
2023-12-08 19:33:22 +0530

    basicreact and vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,13 @@
+import Chai from "./chai"
+
 
 function App() {
   return (
-    <h1>Chai aur react with vite | AS</h1>
+    <>
+      <Chai/>
+      <h1>Chai aur react</h1>
+      <p>test para</p>
+    </>
   )
 }
 
